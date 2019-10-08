f3cc52069edf666fd32d1f59da9114e9d08b64c0
Iván Garay <ivangaray@Ivans-MacBook-Pro.local>
2019-10-08 17:52:09 +0200

    instalacion de bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,10 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
