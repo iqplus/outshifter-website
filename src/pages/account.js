935162fcaff73dfe54c966c3698457b4a8c2e8b5
Ivan Garay <53918691+ivogaray@users.noreply.github.com>
2019-11-01 12:42:09 +0100

    Links forside a Coming Soon

---
+++ src/pages/account.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import Lottie from 'lottie-react-web'
+import Animation1 from '../animations/animation1.json'
+import Animation1Phone from '../images/Animation-1-Phone.png'
+
+const AccountPage = () => (
+  <Layout>
+  <SEO title="Account" />
+  
+    {/* Banner */}
+    <div className={'masthead'}>   
+      <div className={'container ' + 'h-100 '}>
+        <div className="row h-100 align-items-center main-banner no-gutters">
+          <div className="col-md-12 col-lg-5">
+            <h1>Account/Sign in Page<br/>Coming Soon</h1>
+            <p>Page under construction</p>
+          </div>
+          <div className="col-md-12 col-lg-7">
+            <div className="wrapper-animation-1">
+              <div className="animation-background"><img src={Animation1Phone}></img></div>
+              <div className="class-animation"><Lottie options={{animationData: Animation1}}/></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </Layout>
+)
+
+export default AccountPage
