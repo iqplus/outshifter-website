a009c1dec745b8231874a6d8c40ec8dfac93d716
Ivan Garay <53918691+ivogaray@users.noreply.github.com>
2019-11-11 15:32:12 +0100

    Testing para ver si funciona el form

--- src/components/MyContact/MyContact.js
+++ src/components/MyContact/MyContact.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import Button from "../Button/Button"
 import qs from 'qs'
 import * as globalvariables from '../../global'
+
 const FormGroup = styled.div`
     display: block;
     width: 100%;
@@ -33,6 +34,22 @@ const MyInput = styled.input`
     };
 `;
 
+const MySelect = styled.select`
+    background: transparent;
+    padding: 0.5em;
+    margin-top: 0.5em;
+    margin-bottom: 2em;
+    height: 40px;
+    border: none;
+    box-shadow: 0 0 0 1px rgba(50,50,93,.05), 0 2px 5px 0 rgba(50,50,93,.1), 0 1px 1px 0 rgba(0,0,0,.07);
+    border-radius: 6px;
+    width: 100%;
+    &:focus {
+        outline:none;
+        border: 1px solid #4A90E2;
+    };
+`;
+
 const MyTextArea = styled.textarea`
     padding: 0.5em;
     margin-top: 0.5em;
@@ -52,13 +69,25 @@ const MyTextArea = styled.textarea`
 
 class MyContact extends React.Component {
 
-state= {
-    name: 'angelo sepulveda',
-    email: 'angelo.sv@iqplus.no',
-    segment: 'esi',
-    subject: 'sdaoimmds', 
-    message:  'sdfdf sdflk sdk fsldf'    
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            segment: '',
+            email: '',
+            subject: '',
+            message: ''
+      }
+    }
+
+    handleInputChange = event => {
+        const target = event.target
+        const value = target.value
+        const name = target.name
+        this.setState({
+            [name]: value,
+        })
+    }
 
     onFormSubmit = (e) => {
 
@@ -77,37 +106,68 @@ state= {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             }})
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (response) {
-        console.log(response);
-    });
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (response) {
+            console.log(response);
+        });
 
     }
    
     render() {
         return (
             <form onSubmit={this.onFormSubmit} method="post">
-
             <FormGroup>
         
                 <div className="row">
                     <div className="col-md-12 col-lg-6">
                         <Label>Your Name</Label>
-                            <MyInput type="email" name="email" />
+                            <MyInput
+                                type="text"
+                                name="name"
+                                value={this.state.name}
+                                onChange={this.handleInputChange}
+                            />
                     </div>
                     <div className="col-md-12 col-lg-6">
                         <Label>User Segment</Label>
-                        <MyInput type="text" />
+                        <MySelect
+                            name="segment"
+                            value={this.state.segment}
+                            onChange={this.handleInputChange}
+                        >
+                            <option value="" disabled selected>Select a segment</option>
+                            <option value="Mediator">Mediator</option>
+                            <option value="Merchant">Merchant</option>
+                            <option value="Customer">Customer</option>
+                        </MySelect>
                     </div>
                 </div>
+
                 <Label>Email</Label>
-                <MyInput type="text" />
+                <MyInput
+                    type="email"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.handleInputChange}
+                />
+
                 <Label>Subject</Label>
-                <MyInput type="text" />
+                <MyInput
+                    type="text"
+                    name="subject"
+                    value={this.state.subject}
+                    onChange={this.handleInputChange}
+                />
+
                 <Label>Message</Label>
-                <MyTextArea></MyTextArea>
+                <MyTextArea
+                    name="message"
+                    value={this.state.message}
+                    onChange={this.handleInputChange}
+                ></MyTextArea>
+
                 <div className="row submit-button">
                     <div className="col">
                         <Button type="submit">Submit</Button>
@@ -115,9 +175,9 @@ state= {
                 </div>
         
             </FormGroup>
-</form>
+            </form>
         );
     }
 }
 
-export default MyContact
+export default MyContact;
