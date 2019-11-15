0a60caba0f97555efc510107ce424797f6887888
Ivan Garay <53918691+ivogaray@users.noreply.github.com>
2019-11-15 10:05:18 +0100

    developers, algunas correcciones, contact

--- src/components/MyContact/MyContact.js
+++ src/components/MyContact/MyContact.js
@@ -11,6 +11,16 @@ const FormGroup = styled.div`
     margin: 0 auto;
     color: #9C9C9F;
     .submit-button {text-align: right;}
+    .thank-you {
+        text-align: center;
+        border: 1px solid green;
+        border-radius: 6px;
+        margin: 30px 0px;
+    }
+    .thank-you h6 {
+        color: green;
+        margin-top: 8px;
+    }
     @media only screen and (max-width: 991px) {
         .submit-button {text-align: center;}
     }
@@ -76,7 +86,8 @@ class MyContact extends React.Component {
             segment: '',
             email: '',
             subject: '',
-            message: ''
+            message: '',
+            thankyou: false
       }
     }
 
@@ -107,12 +118,28 @@ class MyContact extends React.Component {
                 'Content-Type': 'application/x-www-form-urlencoded'
             }})
         .then(function (response) {
+            console.log("Success");
             console.log(response);
         })
         .catch(function (response) {
+            console.log("Siamo fuori");
             console.log(response);
         });
+        
+        this.setState({
+            thankyou: true
+        })
+        this.resetForm();
+    }
 
+    resetForm() {
+        this.setState({
+            name: '',
+            segment: '',
+            email: '',
+            subject: '',
+            message: ''
+        })
     }
    
     render() {
@@ -173,7 +200,17 @@ class MyContact extends React.Component {
                         <Button type="submit">Submit</Button>
                     </div>
                 </div>
-        
+
+                {this.state.thankyou ? (
+                    <div className="row thank-you">
+                    <div className="col">
+                        <h6>Thank you! Your message has been sent.</h6>
+                    </div>
+                    </div>
+                ) : (
+                    <div></div>
+                )}
+       
             </FormGroup>
             </form>
         );
