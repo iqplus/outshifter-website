aa624cd367a4eb2dac3633f7ac14b3526755893e
Ivan Garay <53918691+ivogaray@users.noreply.github.com>
2020-01-29 17:14:20 -0300

    correcciones forside

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,6 @@ import DotsBackground from '../images/pricing2-min.png'
 
 import Lottie from 'lottie-react-web'
 import LogoBack from '../animations/logo-back.json'
-import LogoRipple from '../animations/logo-ripple-effect.json'
 import BulletsAzulTurquesa from '../animations/bullets-azul-turquesa.json'
 import BulletsAzulVioleta from '../animations/bullets-azul-violeta.json'
 import BulletsTurquesaVioleta from '../animations/bullets-turquesa-violeta.json'
@@ -30,6 +29,28 @@ import BulletsTurquesaAzul from '../animations/bullets-turquesa-azul.json'
 import BulletsVioletaAzul from '../animations/bullets-violeta-azul.json'
 import DotsSphere from '../animations/dots-sphere.json'
 
+const CircleIcon = styled.div`
+  border: none;
+  display: inline-block;
+  border-radius: 50%;
+  color: white;
+  position: relative;
+  z-index: 1;
+
+  padding-top: 2px;
+  padding-bottom: 3px;
+  padding-right: 7px;
+  padding-left: 7px;
+
+  background: #5873e5;
+  background: -moz-linear-gradient(left, #5873e5 0%, #3dcef4 100%);
+  background: -webkit-gradient(linear, left top, right top, color-stop(0%,#5873e5), color-stop(100%,#3dcef4));
+  background: -webkit-linear-gradient(left, #5873e5 0%,#3dcef4 100%);
+  background: -o-linear-gradient(left, #5873e5 0%,#3dcef4 100%);
+  background: -ms-linear-gradient(left, #5873e5 0%,#3dcef4 100%);
+  background: linear-gradient(to right, #5873e5 0%,#3dcef4 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#5873e5', endColorstr='#3dcef4',GradientType=1 );
+`;
 
 const Forside = styled.div`
 .main-banner {
@@ -48,14 +69,79 @@ const Forside = styled.div`
     bottom: 50px;
     right: 50px;
     box-shadow: rgb(226, 226, 226) 1px 1px 5px 2px;
+    svg {
+      -moz-animation: bounce 3s infinite;
+      -webkit-animation: bounce 3s infinite;
+      animation: bounce 3s infinite;
+    }
+    &:hover {
+      box-shadow: rgb(221,223,221) 1px 1px 5px 6px;
+    }
+    p {
+      display: inline;
+      margin-left: 10px;
+    }
     @media only screen and (max-width: 991px) {
       display: none;
     }
   }
-  .how-works p {
-    display: inline;
-    margin-left: 10px;
+}
+@-moz-keyframes bounce {
+  0%, 20%, 50%, 80%, 100% {
+    -moz-transform: translateY(0);
+    transform: translateY(0);
   }
+  40% {
+    -moz-transform: translateY(-10px);
+    transform: translateY(-10px);
+  }
+  60% {
+    -moz-transform: translateY(-5px);
+    transform: translateY(-5px);
+  }
+}
+@-webkit-keyframes bounce {
+  0%, 20%, 50%, 80%, 100% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+  40% {
+    -webkit-transform: translateY(-10px);
+    transform: translateY(-10px);
+  }
+  60% {
+    -webkit-transform: translateY(-5px);
+    transform: translateY(-5px);
+  }
+}
+@keyframes bounce {
+  0%, 20%, 50%, 80%, 100% {
+    -moz-transform: translateY(0);
+    -ms-transform: translateY(0);
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+  40% {
+    -moz-transform: translateY(-10px);
+    -ms-transform: translateY(-10px);
+    -webkit-transform: translateY(-10px);
+    transform: translateY(-10px);
+  }
+  60% {
+    -moz-transform: translateY(-5px);
+    -ms-transform: translateY(-5px);
+    -webkit-transform: translateY(-5px);
+    transform: translateY(-5px);
+  }
+}
+
+.universal h4::before { 
+  display: block; 
+  content: " "; 
+  margin-top: -285px; 
+  height: 285px; 
+  visibility: hidden; 
+  pointer-events: none;
 }
 .magic {
   h4 {
@@ -91,6 +177,7 @@ const Forside = styled.div`
   }
   .universal .logo-animation {
     text-align: center;
+    margin-bottom: 50px;
   }
 }
 /* Dot line animation Universal */
@@ -138,7 +225,7 @@ const Forside = styled.div`
   bottom: 888px;
   left: 108px;
   @media only screen and (max-width: 991px) {
-    display: none;
+    position: ;
   }
 }
 #dot-line-5 {
@@ -173,13 +260,13 @@ const Forside = styled.div`
 #dot-line-8 .c-dashed-line__dash {
   stroke: #3dcef4; /* this must match the background color */
 }
-/* Dot line animation Multichannel */
+/* Dot line animation Outsourcing */
 #dot-outsourcing {
   position: relative;
   bottom: 682px;
   left: 765px;
   @media only screen and (max-width: 991px) {
-    display: inherit;
+    display: none;
   }
 }
 #dot-line-9 {
@@ -263,6 +350,7 @@ const Forside = styled.div`
   margin-right: 15px;
   @media only screen and (max-width: 991px) {
     display: inline-flex;
+    margin-bottom: 15px;
   }
 }
 .multichannel svg {
@@ -302,11 +390,58 @@ const Forside = styled.div`
   right: 0px;
 }*/
 
+.sales-background img {
+  position: absolute;
+  max-width: 500px;
+  opacity: .5;
+  background-repeat: no-repeat;
+  @media only screen and (man-width: 991px) {
+    display: none;
+  }
+}
+
+.sales-background-right img {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  max-width: 500px;
+  opacity: .5;
+  background-repeat: no-repeat;
+  -moz-transform: scaleX(-1);
+  -o-transform: scaleX(-1);
+  -webkit-transform: scaleX(-1);
+  transform: scaleX(-1);
+  filter: FlipH;
+  -ms-filter: "FlipH";
+  @media only screen and (man-width: 991px) {
+    display: none;
+  }
+}
+
 .sales-background-mobile img {
   position: absolute;
   max-width: 400px;
   opacity: .5;
   background-repeat: no-repeat;
+  @media only screen and (min-width: 992px) {
+    display: none;
+  }
+}
+
+.animation-background {
+  position: absolute;
+}
+
+.wrapper-animation-1 {
+  display: flex;
+  align-items: stretch;
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+.wrapper-animation-1 img {
+  max-width: 100%;
+  height: auto;
 }
 
 `;
@@ -384,9 +519,11 @@ class WavesPage extends React.Component {
               </div>
             </div>
           </div>
-          <div className="how-works">
-            <Link to={''}><ButtonCircle><FaAngleDown /></ButtonCircle></Link><p>How outshifting works</p>
-          </div>
+          <Link to={'#row-universal'}>
+            <div className="how-works">
+              <CircleIcon><FaAngleDown /></CircleIcon><p>How outshifting works</p>
+            </div>
+          </Link>  
         </div>
 
         {/* Where the magic begins */}
@@ -442,7 +579,7 @@ class WavesPage extends React.Component {
               </div>
               <div className="col-12 order-1 order-lg-2 col-lg-4">
                 <div className="magic-content">
-                  <h4>Universal Management</h4>
+                  <h4 id="row-universal">Universal Management</h4>
                   <p>
                     Control your entire e-commerce from a single dashboard: With centralized inventory, 
                     outsourcing enablement, order management and more, for physical and digital products.
@@ -541,7 +678,7 @@ class WavesPage extends React.Component {
                 </ul>
               </div>
               <div className="col-lg-4">
-                <div className="magic-content">
+                <div className="magic-content mobile-center">
                   <h4>Outsourcing Network</h4>
                   <p>
                     Exponetially scale distribution trough network effects: Administrate third-party product 
@@ -592,11 +729,10 @@ class WavesPage extends React.Component {
               </svg>
             </div>
 
-            <div className="forsiden-bg-min"></div>
-            <div className="forsiden-bg-min-right"></div>
-
             <div className="row mb-10">
               <div className="col">
+                <div className="sales-background"><img src={DotsBackground} alt="dots backgorund left"/></div>
+                <div className="sales-background-right"><img src={DotsBackground} alt="dots backgorund right"/></div>
                 <div className="sales-background-mobile"><img src={DotsBackground} alt="dots backgorund mobile"/></div>
                 <div className="magic-content mx-auto">
                   <h4>Sales Acceleration</h4>
