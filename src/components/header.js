02c7c2650b4963ae7ceb91ef1dec3a27b283d34d
Ivan Garay <53918691+ivogaray@users.noreply.github.com>
2020-01-30 03:21:13 -0300

    menu moverlo

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,7 @@ const StyledMenuName = styled.span`
     transition: ${({ open }) => open ? 'opacity 0.2s linear 0.1s' : 'opacity 0.2s linear 0.5s'};
     opacity: ${({ open }) => open ? '0' : '1'};
     position: relative;
-    left: 385px;
+    left: 435px;
     line-height: 48px;
     color: #4A4A4A;
     pointer-events: none; 
